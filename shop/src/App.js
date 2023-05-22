fb56198465ff71f65def8eb7282503efdf86ff64
Havi.Hong <havi.hong@kakaovx.com>
2023-05-23 08:40:59 +0900

    이미지up

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,31 +1,24 @@
 import './App.css';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+import BgImage from './img/bg.png' // 코드상에선 이미지도 import
+import shoes1 from './img/shoes1.jpg' 
+
 
 //import Button from 'react-bootstrap/Button'
 //import { Button } from 'react-bootstrap'
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">SHOP</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#HOME">HOME</Nav.Link>
+              <Nav.Link href="#CART">CART</Nav.Link>
             </Nav>
             <Nav>
               <Nav.Link href="#deets">More deets</Nav.Link>
@@ -36,7 +29,22 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </>
+
+      <div className="main-bg" style={{ backgroundImage: 'url(' + BgImage + ')'}}></div>
+
+      <div className='container'>
+        <div className="row">
+          <div className='col-md-4'>
+            <img src="{{ shoes1 }}" />
+            <h4>상품명</h4>
+            <p>상풍설명</p>
+          </div>
+          <div className='col-md-4'>안녕2</div>
+          <div className='col-md-4'>안녕3</div>
+        </div>
+      </div>
+
+    </div>
   );
 }
 
