8a29739578cf72a06f950c1dec712449ea14afdd
Havi.Hong <havi.hong@kakaovx.com>
2023-05-24 16:40:05 +0900

    앱 js 수정

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
+/** 이미지 가져올시 
+ * import 
+ * public 폴더에 넣고 사용
+ *   - process.env.PUBLIC_URL + '/image.jpg' 형태로 사용
+ * 외부 URL
+ **/
+
 import BgImage from './img/bg.png' // 코드상에선 이미지도 import
-import shoes1 from './img/shoes1.jpg' 
 
 
 //import Button from 'react-bootstrap/Button'
@@ -35,12 +41,20 @@ function App() {
       <div className='container'>
         <div className="row">
           <div className='col-md-4'>
-            <img src="{{ shoes1 }}" />
-            <h4>상품명</h4>
+            <img src="/image/shoes1.jpg" width="80%" alt="상품1"/>
+            <h4>상품명1</h4>
             <p>상풍설명</p>
           </div>
-          <div className='col-md-4'>안녕2</div>
-          <div className='col-md-4'>안녕3</div>
+          <div className='col-md-4'>
+            <img src="https://hong-havi.github.io/study-data/img/shoes2.jpg" width="80%" alt="상품2"/>
+            <h4>상품명2</h4>
+            <p>상풍설명</p>
+          </div>
+          <div className='col-md-4'>
+            <img src="https://hong-havi.github.io/study-data/img/shoes3.jpg" width="80%" alt="상품3"/>
+            <h4>상품명3</h4>
+            <p>상풍설명</p>
+          </div>
         </div>
       </div>
 
